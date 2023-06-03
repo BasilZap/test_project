72cfe6c667cbc2957ab1ba86be05086f4950e97f
BasilZap <zapr9@yandex.ru>
2023-06-03 17:03:32 +0300

    Init commit

---
+++ src/main.py
@@ -0,0 +1,37 @@
+class TodoList():
+
+    def __init__(self, task):
+        self.task = task
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.task})'
+
+    def __str__(self):
+        return '\n'.join(self.task)
+
+    def __add__(self, oth):
+        oth = self.task + oth.task
+        obj = TodoList(oth)
+        #oth = self.task + TodoList.task
+        return str(obj) #'\n'.join(oth)
+
+
+tasks = ['Task1', 'Task2']
+list1 = TodoList(tasks)
+
+print(repr(list1))
+# TodoList(list[str])
+
+print(list1)
+# task1
+# task2
+
+list2 = TodoList(['task3', 'task4'])
+
+list3 = list1 + list2
+
+print(list3)
+# task1
+# task2
+# task3
+# task4
